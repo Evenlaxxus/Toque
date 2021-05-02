07149fa1be1ce8dea6d5b3021901c1e57353a7c6
Evenlaxxus <r.ewiak1404@wp.pl>
2021-05-02 13:58:04 +0200

    Adds authentication

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,49 @@
 import React from 'react'
-import app from '../firebase'
+import Signup from './Signup'
+import { AuthProvider } from "../context/AuthContext"
+import { Container } from "react-bootstrap"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import Dashboard from "./Dashboard"
+import Login from './Login'
+
+// function App() {
+//   const [recipes, setRecipes] = React.useState([])
+
+//   React.useEffect(() => {
+//     const fetchData = async () => {
+//       const db = app.firestore()
+//       const data = await db.collection("recipes").get()
+//       setRecipes(data.docs.map(doc => doc.data()))
+//     }
+//     fetchData()
+//   }, [])
+
+//   return (
+//     <ul>
+//       {recipes.map(recipe => (
+//         <li key={recipe.title}>{recipe.title}</li>
+//       ))}
+//     </ul>
+//   )
+// }
 
 function App() {
-  const [recipes, setRecipes] = React.useState([])
-
-  React.useEffect(() => {
-    const fetchData = async () => {
-      const db = app.firestore()
-      const data = await db.collection("recipes").get()
-      setRecipes(data.docs.map(doc => doc.data()))
-    }
-    fetchData()
-  }, [])
-
   return (
-    <ul>
-      {recipes.map(recipe => (
-        <li key={recipe.title}>{recipe.title}</li>
-      ))}
-    </ul>
+      <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}>
+        <div className="w-100" style={{ maxWidth: "400px" }}>
+          <Router>
+            <AuthProvider>
+              <Switch>
+                <Route exact path="/" component={Dashboard} />
+                <Route path="/signup" component={Signup} />
+                <Route path="/login" component={Login} />
+              </Switch>
+            </AuthProvider>
+          </Router>
+        </div>
+      </Container>
   )
 }
 
--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
 import firebase from 'firebase'
+import "firebase/auth"
 
 const config = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -12,5 +13,5 @@ const config = {
 
 firebase.initializeApp(config)
 
-// export const auth = app.auth()
+export const auth = firebase.auth()
 export default firebase;
