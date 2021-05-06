77c700a526ed085c28a18666cde7ebd57b0f0003
Evenlaxxus <r.ewiak1404@wp.pl>
2021-05-06 22:14:34 +0200

    Adds navbar component

--- src/components/App.js
+++ src/components/App.js
@@ -1,39 +1,32 @@
 import React from 'react'
 import Signup from './Signup'
 import {AuthProvider} from "../context/AuthContext"
-import {Container, Nav, Navbar, Row} from "react-bootstrap"
+import {Container, Row} from "react-bootstrap"
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
 import Dashboard from "./Dashboard"
 import Login from './Login'
 import PrivateRoute from "./PrivateRoute";
+import NavbarWrapper from "./NavbarWrapper";
 
 function App() {
 
     return (
-        <React.Fragment>
-            <Navbar bg="dark" variant="dark">
-                <Navbar.Brand href="/">Toque</Navbar.Brand>
-                <Nav className="mr-auto">
-                    <Nav.Link href="/">Login</Nav.Link>
-                    <Nav.Link href="/signup">Signup</Nav.Link>
-                    <Nav.Link href="/dashboard">Dashboard</Nav.Link>
-                </Nav>
-            </Navbar>
-            <Container>
+        <AuthProvider>
+            <NavbarWrapper/>
+            <Container className="pt-4">
                 <Row>
                     <Router>
-                        <AuthProvider>
-                            <Switch>
-                                <Route exact path="/" component={Login}/>
-                                <Route path="/signup" component={Signup}/>
-                                <PrivateRoute path="/dashboard" component={Dashboard}/>
-                                {/*<Route path="/login" component={Login} />*/}
-                            </Switch>
-                        </AuthProvider>
+                        <Switch>
+                            <Route exact path="/" component={Login}/>
+                            <Route path="/signup" component={Signup}/>
+                            <PrivateRoute path="/dashboard" component={Dashboard}/>
+                            {/*<Route path="/login" component={Login} />*/}
+                        </Switch>
                     </Router>
                 </Row>
             </Container>
-        </React.Fragment>
+        </AuthProvider>
+
     )
 }
 
---
+++ src/components/NavbarWrapper.js
@@ -0,0 +1,37 @@
+import React, { useState }  from "react"
+import {Button, Nav, Navbar} from "react-bootstrap";
+import { useAuth } from "../context/AuthContext"
+import { useHistory } from "react-router-dom"
+
+const NavbarWrapper = () => {
+    const [error, setError] = useState("")
+    const { currentUser, logout } = useAuth()
+    const history = useHistory()
+
+    async function handleLogout() {
+        setError("")
+
+        try {
+            await logout()
+            history.push("/")
+        } catch {
+            setError("Failed to log out")
+        }
+    }
+
+
+    const isAuthenticated = !!currentUser
+    return (
+        <Navbar bg="dark" variant="dark">
+            <Navbar.Brand href="/">Toque</Navbar.Brand>
+            <Nav className="mr-auto">
+                {isAuthenticated && (<Nav.Link href="/dashboard">Dashboard</Nav.Link>)}
+            </Nav>
+            {isAuthenticated && (<Button className="mr-sm-2" variant="secondary" onClick={handleLogout}>Logout</Button>)}
+            {!isAuthenticated && (<Button className="mr-sm-2" variant="secondary" href="/">Login</Button>)}
+            {!isAuthenticated && (<Button className="mr-sm-2" variant="secondary" href="/signup">Signup</Button>)}
+        </Navbar>
+    )
+}
+
+export default NavbarWrapper
