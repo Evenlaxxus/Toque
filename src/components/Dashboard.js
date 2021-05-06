ef3b4c00522a8485b5a659059078746b05c107a5
Evenlaxxus <r.ewiak1404@wp.pl>
2021-05-07 00:02:02 +0200

    Adds filtering recipes

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,29 +1,55 @@
 import React from 'react'
 import firebase from '../firebase'
-import {Card, CardColumns, Col, ListGroup} from "react-bootstrap";
+import {Button, Card, CardColumns, Col, Form, FormControl, ListGroup, Row} from "react-bootstrap";
 
 
 export default function Dashboard() {
-    const [recipes, setRecipes] = React.useState([{title: '', ingredients: []}])
+    const [recipes, setRecipes] = React.useState([])
+    const [filtered, setFiltered] = React.useState([])
+
+    function submitHandler(e) {
+        e.preventDefault();
+    }
+
+    function filterRecipes(v) {
+        const query = v.target.value
+        setFiltered(recipes.filter(e => {
+            return e.title.includes(query)
+        }))
+    }
+
     const fetchData = async () => {
         const db = firebase.firestore()
         db.collection("recipes").get()
-            .then(data =>
-                setRecipes(
-                    data.docs.map(doc => doc.data())
-                )
+            .then(data => {
+                    const res = data.docs.map(doc => doc.data())
+                    setRecipes(res)
+                    setFiltered(res)
+                }
             )
     }
+
     React.useEffect(() => {
         fetchData()
     }, [])
 
     return (
         <Col>
+            <Row>
+                <Col className="mb-3">
+                    <Button>
+                        Add recipe
+                    </Button>
+                </Col>
+                <Col xs={12} md={8} className="mb-3">
+                    <Form onSubmit={submitHandler}>
+                        <FormControl type="text" placeholder="Search" onChange={filterRecipes.bind(this)}/>
+                    </Form>
+                </Col>
+            </Row>
             <CardColumns>
-                {recipes.map(recipe => (
+                {filtered.map(recipe => (
                     <Card key={recipe.title}>
-                        <Card.Img variant="top" src="hero.jpg" />
                         <Card.Body>
                             <Card.Title>
                                 {recipe.title}
